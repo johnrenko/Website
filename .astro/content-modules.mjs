67dcd7c00b1389fdc77293e110a8b8c79cbb19d8
johnrenko <john.dutamby@gmail.com>
2025-10-17 13:26:10 +0200

    Refactor content module order and update font in HeroHeader and BaseLayout components to 'Ovo' for improved typography consistency.

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,5 @@
 
 export default new Map([
-["src/content/work/livi-fr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Flivi-fr.mdx&astroContentModuleFlag=true")],
-["src/content/experiments/gradient-card.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fexperiments%2Fgradient-card.mdx&astroContentModuleFlag=true")]]);
+["src/content/experiments/gradient-card.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fexperiments%2Fgradient-card.mdx&astroContentModuleFlag=true")],
+["src/content/work/livi-fr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Flivi-fr.mdx&astroContentModuleFlag=true")]]);
 		
