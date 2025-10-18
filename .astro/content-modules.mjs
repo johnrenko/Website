4acc16a09ea97353bd3084b66615bd0cc044b271
johnrenko <john.dutamby@gmail.com>
2025-10-18 16:37:59 +0200

    Refactor content modules and enhance work page layout
    - Rearranged content module imports for 'paladin-fr.mdx' and 'quitoxil-fr.mdx' for better organization.
    - Updated the layout of the work page to include previous/next navigation and improved header structure.
    - Added scroll position restoration functionality to enhance user experience when navigating between pages.

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
 ["src/content/work/livi-fr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Flivi-fr.mdx&astroContentModuleFlag=true")],
-["src/content/work/quitoxil-fr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fquitoxil-fr.mdx&astroContentModuleFlag=true")],
-["src/content/work/paladin-fr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fpaladin-fr.mdx&astroContentModuleFlag=true")]]);
+["src/content/work/paladin-fr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fpaladin-fr.mdx&astroContentModuleFlag=true")],
+["src/content/work/quitoxil-fr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fquitoxil-fr.mdx&astroContentModuleFlag=true")]]);
 		
