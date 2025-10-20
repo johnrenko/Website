de231fe0aa89ded028b72e4a242a19f67b859ff1
johnrenko <john.dutamby@gmail.com>
2025-10-21 00:04:43 +0200

    Enhance content structure and update work entries
    - Added new fields for company and role descriptions in content configuration.
    - Updated work entries for 'Livi', 'Paladin', and 'Quitoxil' to include company details and role descriptions.
    - Introduced a new entry for 'Dep.Part.' in the content modules.
    - Improved layout and styling for the work and contact sections, including a scroll header and animated separators.

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,8 @@
 
 export default new Map([
-["src/content/work/livi-fr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Flivi-fr.mdx&astroContentModuleFlag=true")],
 ["src/content/work/paladin-fr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fpaladin-fr.mdx&astroContentModuleFlag=true")],
-["src/content/work/quitoxil-fr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fquitoxil-fr.mdx&astroContentModuleFlag=true")]]);
+["src/content/work/quitoxil-fr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fquitoxil-fr.mdx&astroContentModuleFlag=true")],
+["src/content/work/livi-fr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Flivi-fr.mdx&astroContentModuleFlag=true")],
+["src/content/work/deppart-fr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fdeppart-fr.mdx&astroContentModuleFlag=true")],
+["src/content/work/zparse-fr.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fzparse-fr.mdx&astroContentModuleFlag=true")]]);
 		
